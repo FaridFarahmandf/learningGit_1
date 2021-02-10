9bb56876e38707a967d6b1a5ce1af9e9ef79877b
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-02-10 21:15:03 +0330

    create script and add button to remove big Part

---
+++ main/JS/script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn_remove').addEventListener('click' , () => {
+    document.getElementById('part').style.width = '0' ;
+    document.getElementById('part').style.height = '0' ;
+    document.getElementById('part').style.transition = 'all .5 s'
+
+})
